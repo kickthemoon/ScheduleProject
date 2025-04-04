a4104f4fa1bcdaec4d3136039d6742657860fc5a
killthemoon <soonch67890@gmail.com>
2025-04-04 12:11:16 +0900

    빠진 부분 수정 > SchduleRquestDto.Class

--- src/main/java/com/study/schedule/schdule/dto/ScheduleRequestDto.java
+++ src/main/java/com/study/schedule/schdule/dto/ScheduleRequestDto.java
@@ -1,12 +1,22 @@
 package com.study.schedule.schdule.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 
 @Getter
 public class ScheduleRequestDto {
+
+    @NotBlank
     private final String title;
+
     private final String contents;
+
+    @NotNull
+    @Size(min = 4, max = 13)
     private final String passowrd;
+
     private final Long userId;
 
     public ScheduleRequestDto(String title, String contents, String password, Long userId) {
